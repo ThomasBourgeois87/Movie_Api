3c597efb4a9c598c4f9ebe7ede1dbb4c271b90f0
Thomas Bourgeois <thomas.bourgeois@etu.unilim.fr>
2022-02-07 10:40:24 +0100

    insertion user dans bd ok

--- server/manifest.js
+++ server/manifest.js
@@ -35,25 +35,24 @@ module.exports = new Confidence.Store({
                 options: {}
             },
             {
-                plugin: '@hapipal/schwifty',
-                options: {
-                    $filter: 'NODE_ENV',
-                    $default: {},
-                    $base: {
-                        migrateOnStart: true,
-                        knex: {
-                            client: 'sqlite3',
-                            useNullAsDefault: true,     // Suggested for sqlite3
-                            connection: {
-                                filename: ':memory:'
-                            },
-                            migrations: {
-                                stub: Schwifty.migrationsStubPath
+                plugin  : '@hapipal/schwifty',
+                options : {
+                    $filter    : 'NODE_ENV',
+                    $default   : {},
+                    $base      : {
+                        migrateOnStart : true,
+                        knex           : {
+                            client     : 'mysql',
+                            connection : {
+                                host     : process.env.DB_HOST || '0.0.0.0',
+                                user     : process.env.DB_USER || 'root',
+                                password : process.env.DB_PASSWORD || '',
+                                database : process.env.DB_DATABASE || 'nodejs'
                             }
                         }
                     },
-                    production: {
-                        migrateOnStart: false
+                    production : {
+                        migrateOnStart : false
                     }
                 }
             },
